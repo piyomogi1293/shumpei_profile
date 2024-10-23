fccfe41f282e98516188eac9d5b746789cf1ca17
piyomogi1293 <yomoyomo1293@gmail.com>
2024-10-23 10:20:01 +0900

    技術要素の更新など

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shunpei Portfolio",
+  title: "Shumpei Portfolio",
   description: "Shumpei Kido's accomplishments",
 };
 
